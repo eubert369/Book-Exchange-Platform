7fe541e8c08b5848edb6642f669a290b3d7b2be4
eubert369 <marceubert886@gmail.com>
2023-05-26 14:48:56 +0800

    added template login page

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Login</title>
+</head>
+
+<body class="bg-dark container-fluid">
+    <h1 class="text-white text-center mt-4 mb-5">Book Exchange Platform</h1>
+    <div class="row">
+        <div class="container border col-6 mx-auto text-center rounded-5 p-3 mt-5">
+            <h3 class="text-white fs-2">Login</h3>
+            <form action="#">
+                <input class="rounded-3 p-2 w-75 mt-2" type="text" placeholder="Username">
+                <input class="rounded-3 p-2 w-75 mt-3" type="password" placeholder="Password">
+                <div class="col-auto mt-4 mb-3">
+                    <button class="btn btn-primary" type="submit">Login</button>
+                </div>
+                <p class="text-white"><a href="#">Signup</a> if you don't have an account.</p>
+            </form>
+        </div>
+    </div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+
+</html>
