85fd7b4b4bc413b7c6104e62f5f833558e938bae
eubert369 <marceubert886@gmail.com>
2023-05-26 15:07:38 +0800

    title updated

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
     <title>Login</title>
 </head>
 
-<body class="bg-dark container-fluid">
-    <h1 class="text-white text-center mt-4 mb-5">Book Exchange Platform</h1>
+<body class="bg-dark container">
+    <h1 class="text-white text-center mt-4 mb-5">Unlocking the Literary Community: Developing a Book Exchange Network</h1>
     <div class="row">
         <div class="container border col-6 mx-auto text-center rounded-5 p-3 mt-5">
             <h3 class="text-white fs-2 mb-3">Login</h3>
@@ -18,13 +18,14 @@
                 <input class="rounded-3 p-2 w-75 mt-2" type="text" placeholder="Username">
                 <input class="rounded-3 p-2 w-75 mt-3" type="password" placeholder="Password">
                 <div class="col-auto mt-4 mb-3">
-                    <button class="btn btn-primary" type="submit">Login</button>
+                    <button class="btn bg-white" type="submit">Login</button>
                 </div>
                 <p class="text-white"><a href="#">Signup</a> if you don't have an account.</p>
             </form>
         </div>
     </div>
 </body>
+
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
 
