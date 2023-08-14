3da604ca78e9636ca707daf529dab774780f139a
MEC369 <marceubert886@gmail.com>
2023-08-14 23:33:51 +0800

    initial update

---
+++ components/aboutUs.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BookLoopPH | About Us</title>
+    <link rel="stylesheet" href="../bootstrap/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container-fluid p-0">
+        <div class="row mt-5" style="color: #8A6342;">
+            <div class="col px-4">
+                <h2 class="fw-bold text-center">Mission Statement</h2>
+                <p class="mx-5" style="text-align: justify;">Out mission is to create a vibrant online marketplace that connects students and book enthusiasts, providing them with a seamless platform to buy, sell, rent, and exchange used books. We aim to foster a community-driven environment that promotes accessibility, affordability, and sustainability in world of literature. By fascilitating the circulation of knowledge and promoting the reuse of books, we strive to empower individuals in their pursuit of education, personal growth, and the joy of reading.</p>
+            </div>
+            <div class="col text-center">
+                <h2 class="fw-bold">Vision Statement</h2>
+                <p class="mx-5" style="text-align: justify;">Our vision is to revolutionize the way students and book enthusiasts access and engage with used books. We envision a future where our platform becomes the go-to destination for anyone seeking affordable and accessible literary resources. Through out website, we aspire to create a global netwirk of passionate readers and learners, fostering connections and promoting the sharing of ideas and stories. By harnessing the power of technology and the collective enthusiasm for books, we aim to transform the way people discover, acquire, and exchange, knowledge, fostering a more sustainable and inclusive rading community.</p>
+            </div>
+        </div>
+        <div class="row text-center mt-4 mx-2 mb-5" style="color: #8A6342;">
+            <div class="col mx-5 rounded-4 p-3 border" style="background-color: #F2F1EE;">
+                <i class="w-100 text-center"><img src="../assets/find books.png" alt="icon" width="150" height="110"></i>
+                <h4 class="fw-bold mt-3">Find Books</h4>
+                <p style="text-align: justify;">Use our search boxes to find your desired book - you can search by author, title, ISBN, or any keyword. Our advanced search helps you find books by other key criteria including price, publisher, publishing date, bookseller location and more.</p>
+                <a class="text-decoration-none fw-bold fs-5" href="#" style="color: #8A6342;">Search for books <i><img src="../assets/next.png" alt="icon"></i></a>
+            </div>
+            <div class="col mx-5 rounded-4 p-3 border" style="background-color: #F2F1EE;">
+                <i class="w-100 text-center"><img src="../assets/buy with confidence.png" alt="icon" width="150" height="110"></i>
+                <h4 class="fw-bold mt-3">Buy with Confidence</h4>
+                <p style="text-align: justify;">Your order is shipped directly from the bookseller. We guarantee the condition of every book as it is described on our website. You are eligible for a refund within 1 week if dissatisfied due to a bookseller error or if your ook fails to arrive.</p>
+                <a class="text-decoration-none fw-bold fs-5" href="#" style="color: #8A6342;">Learn about our return policy <i><img src="../assets/next.png" alt="icon"></i></a>
+            </div>
+            <div class="col mx-5 rounded-4 p-3 position-relative border" style="background-color: #F2F1EE;">
+                <i class="w-100 text-center"><img src="../assets/secure shopping.png" alt="icon" width="150" height="110"></i>
+                <h4 class="fw-bold mt-3">Secure Shopping</h4>
+                <p style="text-align: justify;">BookLoopPH is a secure website for online shopping. Your personal information is protected at all times, plus our booksellers follow strict security policies.</p>
+                <a class="text-decoration-none fw-bold fs-5 position-absolute bottom-0 start-0 mb-3 w-100" href="#" style="color: #8A6342;">Privacy & security <i><img src="../assets/next.png" alt="icon"></i></a>
+            </div>
+        </div>
+    </div>
+</body>
+<script src="../bootstrap/bootstrap.bundle.min.js"></script>
+
+</html>
